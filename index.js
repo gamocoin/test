9eb16c4639191f976afc26a82385157c6112d2b7
gamocoin <gamocoin.io@gmail.com>
2025-09-15 13:28:01 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-console.log("Hello world formthe zenkings.com")
-console.log("Hello world formthe zenkings.com")
-console.log("Hello world formthe zenkings.com")
-console.log("Hello world formthe zenkings.com")
-console.log("Hello world formthe zenkings.com")
-console.log("Hello world formthe zenkings.com")
-console.log("Hello world formthe zenkings.com")
-console.log("Hello world formthe zenkings.com")
 
+if (z ==12) console.log("applee is niche")
 
 let x =10;
 
 if(x) console.log("errore"
 )
+console.log(12123213)
+
 
 
 let y = 10;
 if (y) {
     console.log("false wrigjt");
 }
+
