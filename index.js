b442419009e5c52327b7bc61df827d3235bd0eda
webowares1012 <webowares@gmail.com>
2025-09-15 12:41:00 +0530

    second commit

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ console.log("Hello world formthe zenkings.com")
 console.log("Hello world formthe zenkings.com")
 console.log("Hello world formthe zenkings.com")
 console.log("Hello world formthe zenkings.com")
+
+
+let y = 10;
+if (y) {
+    console.log("false wrigjt");
+}
