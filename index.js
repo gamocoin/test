1e52c92c93d80852a547ad271db45c287a91ec95
gamocoin <gamocoin.io@gmail.com>
2025-09-15 12:48:58 +0530

    amit 2 commit

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ console.log("Hello world formthe zenkings.com")
 console.log("Hello world formthe zenkings.com")
 console.log("Hello world formthe zenkings.com")
 console.log("Hello world formthe zenkings.com")
+
+
+let x =10;
+
+if(x) console.log("errore"
+)
